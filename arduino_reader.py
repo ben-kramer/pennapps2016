575a0d4cb2df8634b41fca66d53654d5bb56ee41
Guillermo <gguti@seas.upenn.edu>
2016-01-23 01:36:31 -0500

    Created reader file

---
+++ arduino_reader.py
@@ -0,0 +1,10 @@
+"""
+Author: Guillermo Gutierrez
+Date: January 23, 2016
+Version: 1
+Description: This file reads in the data from the arduino and returns it
+"""
+import serial
+
+def arduino_read():
+    pass
